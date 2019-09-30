f1cef5ff8cca16c16bcc7fbc50924ed482791374
kevin <kevinb2002@live.nl>
2019-09-30 12:14:24 +0200

    added point on intersection

--- html/06Intersectinglines/script.js
+++ html/06Intersectinglines/script.js
@@ -7,11 +7,11 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
-let A = new Point(new Vector2d(200,200),15,"green","A",true);
-let B = new Point(new Vector2d(600,600),15,"blue","A",true);
-let C = new Point(new Vector2d(200,600),15,"pink","A",true);
-let D = new Point(new Vector2d(600,200),15,"purple","A",true);
-let I = new Point(new Vector2d(400,400),10,"white","A",false);
+let A = new Point(new Vector2d(200,200),150,"green","A",true);
+let B = new Point(new Vector2d(600,600),150,"blue","A",true);
+let C = new Point(new Vector2d(200,600),150,"pink","A",true);
+let D = new Point(new Vector2d(600,200),150,"purple","A",true);
+let I = new Point(new Vector2d(400,450),10,"white","A",true);
 
 let l = new LinearFunction(1,1);
 let m = new LinearFunction(1,1);
@@ -21,9 +21,15 @@ function animate()
   requestAnimationFrame(animate);
   context.clearRect(0,0, width,height);
   l.defineLineWithTwoPoints(A,B);
-  m.defineLineWithTwoPoints(C,D);
   l.draw(context);
+
+  I.position.dx = l.intersection(m).x;
+  I.position.dy = l.intersection(m).y;
+
+  m.defineLineWithTwoPoints(C,D);
   m.draw(context);
+
+
   A.draw(context);
   B.draw(context);
   C.draw(context);
