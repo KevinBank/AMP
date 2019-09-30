e5b1a4491b70beffe50ebbbab9e24cfc6856759e
kevin <kevinb2002@live.nl>
2019-09-30 11:39:19 +0200

    Update script.js

--- html/06Intersectinglines/script.js
+++ html/06Intersectinglines/script.js
@@ -11,6 +11,7 @@ let A = new Point(new Vector2d(200,200),15,"green","A",true);
 let B = new Point(new Vector2d(600,600),15,"blue","A",true);
 let C = new Point(new Vector2d(200,600),15,"pink","A",true);
 let D = new Point(new Vector2d(600,200),15,"purple","A",true);
+let I = new Point(new Vector2d(400,400),10,"white","A",false);
 
 let l = new LinearFunction(1,1);
 let m = new LinearFunction(1,1);
@@ -27,6 +28,7 @@ function animate()
   B.draw(context);
   C.draw(context);
   D.draw(context);
+  I.draw(context);
 }
 
 animate();
