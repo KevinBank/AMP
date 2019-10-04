e313f2b0303444ce0f85bdf5b9b2beef0bd9d3c5
kevin <kevinb2002@live.nl>
2019-10-04 15:38:25 +0200

    Duplicated 06

---
+++ html/07PerpendicularLines/script.js
@@ -0,0 +1,40 @@
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+canvas.width = width;
+canvas.height = height;
+
+let A = new Point(new Vector2d(200,200),150,"green","A",true);
+let B = new Point(new Vector2d(600,600),150,"blue","A",true);
+let C = new Point(new Vector2d(200,600),150,"pink","A",true);
+let D = new Point(new Vector2d(600,200),150,"purple","A",true);
+let I = new Point(new Vector2d(400,450),10,"white","A",true);
+
+let l = new LinearFunction(1,1);
+let m = new LinearFunction(1,1);
+
+function animate()
+{
+  requestAnimationFrame(animate);
+  context.clearRect(0,0, width,height);
+  l.defineLineWithTwoPoints(A,B);
+  l.draw(context);
+
+  I.position.dx = l.intersection(m).x;
+  I.position.dy = l.intersection(m).y;
+
+  m.defineLineWithTwoPoints(C,D);
+  m.draw(context);
+
+
+  A.draw(context);
+  B.draw(context);
+  C.draw(context);
+  D.draw(context);
+  I.draw(context);
+}
+
+animate();
