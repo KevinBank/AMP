87fa173bc0246809d948860ef8dfac5aeb5315a8
kevin <kevinb2002@live.nl>
2019-10-07 12:23:09 +0200

    Update script.js

--- html/08mediarAndCentroid/script.js
+++ html/08mediarAndCentroid/script.js
@@ -7,19 +7,55 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
-let A = new Point(new Vector2d(200,300),25,"pink","A",true);
-let B = new Point(new Vector2d(300,200),25,"pink","A",true);
-let C = new Point(new Vector2d(300,300),25,"pink","A",true);
+let A = new Point(new Vector2d(200,500),25,"purple","A",true);
+let B = new Point(new Vector2d(600,200),25,"purple","A",true);
+let C = new Point(new Vector2d(500,500),25,"purple","A",true);
 
+let ABm = new Point(new Vector2d(0,0),15,"white","A",false);
+let BCm = new Point(new Vector2d(0,0),15,"white","A",false);
+let CAm = new Point(new Vector2d(0,0),15,"white","A",false);
+
+let lab = new LinearFunction(1,1);
+let lbc = new LinearFunction(1,1);
+let lca = new LinearFunction(1,1);
+
+let labmc = new LinearFunction(1,1);
+let lbcma = new LinearFunction(1,1);
+let lcamb = new LinearFunction(1,1);
 
 function animate()
 {
   context.clearRect(0,0,width,height);
   requestAnimationFrame(animate);
 
+  lab.defineLineWithTwoPoints(A,B);
+  lab.draw(context);
+  lbc.defineLineWithTwoPoints(B,C);
+  lbc.draw(context);
+  lca.defineLineWithTwoPoints(C,A);
+  lca.draw(context);
+
+  labmc.defineLineWithTwoPoints(ABm,C);
+  labmc.draw(context);
+  lbcma.defineLineWithTwoPoints(BCm,A);
+  lbcma.draw(context);
+  lcamb.defineLineWithTwoPoints(CAm,B);
+  lcamb.draw(context);
+
+  ABm.position.dx = (A.position.dx + B.position.dx)/2;
+  ABm.position.dy = (A.position.dy + B.position.dy)/2;
+  BCm.position.dx = (B.position.dx + C.position.dx)/2;
+  BCm.position.dy = (B.position.dy + C.position.dy)/2;
+  CAm.position.dx = (C.position.dx + A.position.dx)/2;
+  CAm.position.dy = (C.position.dy + A.position.dy)/2;
+
   A.draw(context);
   B.draw(context);
   C.draw(context);
+
+  ABm.draw(context);
+  BCm.draw(context);
+  CAm.draw(context);
 }
 
 animate()
